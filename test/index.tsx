1662a4554b8b166a32b469ffc4b404076544d6ef
Tuan Pham <neves.pham@gmail.com>
2021-10-15 09:57:10 +0800

    prettier

--- test/index.tsx
+++ test/index.tsx
@@ -1,13 +1,13 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement } from 'react';
 import {
     render as baseRender,
     RenderOptions,
     RenderResult,
-} from "@testing-library/react";
+} from '@testing-library/react';
 
-import { QueryClient, QueryClientProvider } from "react-query";
-import { Provider } from "react-redux";
-import store from "@redux/store";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
+import store from '@redux/store';
 
 /**
  * Custom renderer example with @testing-library/react
@@ -29,11 +29,11 @@ export const AllTheProviders = ({ children }) => {
     );
 };
 
-const render = (ui: ReactElement, options?: Omit<RenderOptions, "queries">) =>
+const render = (ui: ReactElement, options?: Omit<RenderOptions, 'queries'>) =>
     baseRender(ui, { wrapper: AllTheProviders, ...options }) as RenderResult;
 
 // re-export everything
-export * from "@testing-library/react";
+export * from '@testing-library/react';
 
 // override render method
 export { render };
